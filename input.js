4262e18b8b445554a4d05bf30fe5075ca7c6e016
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-01 11:26:21 -0800

    added addEventListener to window on keydown event

--- input.js
+++ input.js
@@ -4,6 +4,23 @@ let inputDirection = {
 }
 
 
+window.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'ArrowUp':
+      inputDirection = { x: 0, y: -1 };
+      break; 
+    case 'ArrowDown':
+      inputDirection = { x: 0, y: 1 };
+      break 
+    case 'ArrowLeft': 
+      inputDirection = { x: -1, y: 0 };
+      break 
+    case 'ArrowRight': 
+      inputDirection = { x: 1, y: 0 };
+      break 
+  }
+})
+
 export function getDirection() {
   return inputDirection
 } 
