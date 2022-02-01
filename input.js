8264001398e81bdaa20fc54c8370a955694cb67e
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-01 11:42:39 -0800

    added if checks to prevent snake go from left to right and up to down

--- input.js
+++ input.js
@@ -3,24 +3,35 @@ let inputDirection = {
   y: 0, 
 }
 
+let lastInputDirection = {
+  x: 0,
+  y: 0,
+};
+
 
 window.addEventListener('keydown', event => {
   switch (event.key) {
     case 'ArrowUp':
+      if (lastInputDirection.y !== 0) break 
       inputDirection = { x: 0, y: -1 };
       break; 
     case 'ArrowDown':
+      if (lastInputDirection.y !== 0) break 
       inputDirection = { x: 0, y: 1 };
       break 
     case 'ArrowLeft': 
+      if (lastInputDirection.x !== 0) break 
       inputDirection = { x: -1, y: 0 };
       break 
     case 'ArrowRight': 
+      if (lastInputDirection.x !== 0) break 
       inputDirection = { x: 1, y: 0 };
       break 
   }
 })
 
 export function getDirection() {
+  lastInputDirection = inputDirection 
+  console.log(lastInputDirection);
   return inputDirection
 } 
--- snake.js
+++ snake.js
@@ -2,16 +2,15 @@ import { getDirection } from "./input.js";
 
 export const SNAKE_SPEED = 2;
 const snakeBody = [
+  { x: 10, y: 11 },
   { x: 11, y: 11 },
 ]; 
-
 
 export function update() {
   const inputDirection = getDirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
       snakeBody[i + 1] = { ...snakeBody[i] };
   }
-
   snakeBody[0].x += inputDirection.x
   snakeBody[0].y += inputDirection.y
 }
