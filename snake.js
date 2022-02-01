701e9b4600067b4c6bcc1e5de86cf6898f9354e7
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-01 11:02:03 -0800

    added the update function

--- snake.js
+++ snake.js
@@ -1,11 +1,16 @@
 export const SNAKE_SPEED = 2;
+const snakeBody = [
+  { x: 11, y: 11 },
+]; 
 
-const snakeBody = [{ x:11, y:11 }] 
 
 export function update() {
- 
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+      snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+  snakeBody[0].x += 1 
+  snakeBody[0].y += 0 
 }
-
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
