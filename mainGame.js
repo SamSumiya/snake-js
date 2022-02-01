8bc360bd2a3196e0a2714db87a75f8a30c3a78b4
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-02-01 08:48:20 -0800

    added a new file of snake and exported the speed to mainGame file

--- mainGame.js
+++ mainGame.js
@@ -1,21 +1,27 @@
 
-
+import { SNAKE_SPEED } from './snake'
 
 
 // Setup the game loop so that I can update the status 
 
 let lastRenderTime = 0
-const SNAKE_SPEED = 2
+
 
 function main(currentTime) {
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-
-  console.log('Render!')
   lastRenderTime = currentTime; 
-  console.log(secondsSinceLastRender);
 
+  update() 
+  draw() 
 }
 
 window.requestAnimationFrame(main);
+
+
+function update() {
+
+}
+
+function draw() {}
---
+++ snake.js
@@ -0,0 +1,1 @@
+const SNAKE_SPEED = 2;
